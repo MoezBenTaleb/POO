34d219061b63ace95983f89084ae97972b2ca626
MoezBenTaleb <103032439+MoezBenTaleb@users.noreply.github.com>
2022-04-10 23:45:39 +0100

    Update Voile.java

--- Voile.java
+++ Voile.java
@@ -3,7 +3,7 @@ public class Voile {
     private String forme;
     private float epaisseur;
     public String couleur;
-    protected int hauteur;
+    protected float hauteur;
 
     private boolean enroule;
     private boolean deroule;
@@ -11,6 +11,8 @@ public class Voile {
    final int ID = 5;
 
   public Voile(){
+     
+     this.couleur = "Blanc";
     
     }
     
@@ -28,7 +30,7 @@ public class Voile {
 
   public void enrouler(boolean enroule){
    
-	this.enroule = enroule;
+   this.enroule = enroule;
   
    }
 
@@ -38,9 +40,56 @@ public class Voile {
    }
 
    public int getID() {
-      return this.ID;
+      return ID;
     }
 
+  public String getForme(){
+    return forme;
+   }
+
+  public void setForme(String forme){
+   this.forme = forme;
+   }
+
+  public float getEpaisseur(){
+    return epaisseur;
+   }
+ 
+  public void setEpaisseur(float Epaisseur){
+   this.epaisseur = epaisseur;
+   }
+
+  public String getCouleur() {
+   return couleur;
+   }
+
+  public void setCouleur(String couleur) {
+    this.couleur = couleur;
+   }
+
+  public float getHauteur() {
+    return hauteur;
+   }
+
+  public void setHauteur(float hauteur) {
+   this.hauteur = hauteur;
+   }
+
+  public boolean getEnroule() {
+    return enroule;
+   }
+
+   public void setEnroule(boolean enroule) {
+    this.enroule = enroule;
+   }
+
+   public boolean getDeroule() {
+     return deroule;
+   }
+
+   public void setDeroule(boolean deroule) {
+    this.deroule = deroule;
+   }
 
      public String toString() {
       return "La voile n°" + this.ID;
